d6ed4c97dd98737caa18138cd1531be38088e873
huang-zhongyi <790209714@qq.com>
2017-11-18 12:37:25 +0800

    add all_default.sh

---
+++ all_default.sh
@@ -0,0 +1,12 @@
+source activate tensorflow
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64/
+#pip install -U pip
+#pip install -r requirements.txt
+nvidia-smi
+python train1.py a0001
+python eval1.py a0001
+python train2.py a0001
+python eval2.py a0001
+python convert.py a0002 
+tensorboard --logdir=./logdir
+
